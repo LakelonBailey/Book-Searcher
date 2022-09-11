5a874715e2036d323282fab4b283338fcb0fa13e
OutliersAdvantage <102266508+LakelonBailey@users.noreply.github.com>
2022-09-11 16:58:45 -0400

    completed resolvers file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,19 +40,17 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
-        // saveBook: async (parent, args, context) => {
-        //     if (context.user) {
-        //         const updatedUser = await User.findOneAndUpdate(
-        //             { _id: user._id },
-        //             { $addToSet: { savedBooks: body } },
-        //             { new: true, runValidators: true }
-        //             );
-      
-        //       return thought;
-        //     }
-      
-        //     throw new AuthenticationError('You need to be logged in!');
-        //   },
+        saveBook: async (parent, { bookBody }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: bookBody } },
+                    { new: true, runValidators: true }
+                    );
+              return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
     }
 
 
